9ea128e8205c4f6f1f0a63f6618006cc58b10c40
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 13:07:45 -0800

    use autoreconf

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,7 @@ export DISABLE_AUTOBREW=1
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
     sed -i "s?\${R_HOME}?${BUILD_PREFIX}?" configure.ac
+    autoreconf --force --verbose --install
 fi
 
 $R CMD INSTALL --build . ${R_ARGS:-}
