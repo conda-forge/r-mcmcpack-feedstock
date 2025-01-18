969811cb48a37195f41483016b1fe8b8632ff901
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 13:18:10 -0800

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,9 +4,9 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 export DISABLE_AUTOBREW=1
 
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-    sed -i "s?\${R_HOME}?${BUILD_PREFIX}?" configure.ac
-    autoreconf --force --verbose --install
-fi
+# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+#     sed -i "s?\${R_HOME}?${BUILD_PREFIX}?" configure.ac
+#     autoreconf --force --verbose --install
+# fi
 
 $R CMD INSTALL --build . ${R_ARGS:-}
